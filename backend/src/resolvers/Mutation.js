01c5cb9437ee0c7b389adced2e7b7fd81aa008fc
Evan Burrell <burrelle@oregonstate.edu>
2018-12-17 14:43:55 +0000

    Password reset flow

--- backend/src/resolvers/Mutation.js
+++ backend/src/resolvers/Mutation.js
@@ -1,10 +1,10 @@
 const bcrypt = require('bcryptjs');
 const jwt = require('jsonwebtoken');
+const { randomBytes } = require('crypto');
+const { promisify } = require('util');
 
 const Mutations = {
   async createItem(parent, args, ctx, info) {
-    // TODO: Check if they are logged in
-
     const item = await ctx.db.mutation.createItem(
       {
         data: {
@@ -13,17 +13,11 @@ const Mutations = {
       },
       info
     );
-
-    console.log(item);
-
     return item;
   },
   updateItem(parent, args, ctx, info) {
-    // first take a copy of the updates
     const updates = { ...args };
-    // remove the ID from the updates
     delete updates.id;
-    // run the update method
     return ctx.db.mutation.updateItem(
       {
         data: updates,
@@ -36,11 +30,7 @@ const Mutations = {
   },
   async deleteItem(parent, args, ctx, info) {
     const where = { id: args.id };
-    // 1. find the item
     const item = await ctx.db.query.item({ where }, `{ id title}`);
-    // 2. Check if they own that item, or have the permissions
-    // TODO
-    // 3. Delete it!
     return ctx.db.mutation.deleteItem({ where }, info);
   },
   async signUp(parent, args, ctx, info) {
@@ -83,6 +73,49 @@ const Mutations = {
   signout(parent, args, ctx, info) {
     ctx.response.clearCookie('token');
     return { message: 'Signed out' };
+  },
+  async requestReset(parent, args, ctx, info) {
+    const user = await ctx.db.query.user({ where: { email: args.email } });
+    if (!user) {
+      throw new Error(`No such user found for email ${args.email}`);
+    }
+    const randomBytesPromiseified = promisify(randomBytes);
+    const resetToken = (await randomBytesPromiseified(20)).toString('hex');
+    const resetTokenExpiry = Date.now() + 360000
+    const res = await ctx.db.mutation.updateUser({
+      where: { email: args.email },
+      data: { resetToken, resetTokenExpiry }
+    });
+    return { message: 'Thanks!' };
+  },
+  async resetPassword(parent, args, ctx, info) {
+    if (args.password !== args.confirmPassword) {
+      throw new Error("Yo Passwords don't match!");
+    }
+    const [user] = await ctx.db.query.users({
+      where: {
+        resetToken: args.resetToken,
+        resetTokenExpiry_gte: Date.now() - 3600000
+      }
+    });
+    if (!user) {
+      throw new Error('This token is either invalid or expired!');
+    }
+    const password = await bcrypt.hash(args.password, 10);
+    const updatedUser = await ctx.db.mutation.updateUser({
+      where: { email: user.email },
+      data: {
+        password,
+        resetToken: null,
+        resetTokenExpiry: null
+      }
+    });
+    const token = jwt.sign({ userId: updatedUser.id }, process.env.APP_SECRET);
+    ctx.response.cookie('token', token, {
+      httpOnly: true,
+      maxAge: 1000 * 60 * 60 * 24 * 365
+    });
+    return updatedUser;
   }
 };
 
--- frontend/pages/signup.js
+++ frontend/pages/signup.js
@@ -1,5 +1,6 @@
 import Signup from '../components/Signup';
 import Signin from '../components/Signin';
+import RequestReset from '../components/RequestReset';
 import styled from 'styled-components';
 
 const Columns = styled.div`
@@ -8,11 +9,12 @@ const Columns = styled.div`
   grid-gap: 20px;
 `;
 
-export default function SignupPage() {
-  return (
-    <Columns>
-      <Signup />
-      <Signin />
-    </Columns>
-  );
-}
+const SignupPage = props => (
+  <Columns>
+    <Signup />
+    <Signin />
+    <RequestReset />
+  </Columns>
+);
+
+export default SignupPage;
