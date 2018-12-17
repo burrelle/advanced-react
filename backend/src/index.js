23e7683964fcb3ca9c0f94674866ce48f3f1b057
Evan Burrell <burrelle@oregonstate.edu>
2018-12-17 11:23:47 +0000

    Currently logged in user with middleware and render props

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,11 +1,19 @@
-const cookieParser = require('cookie-parser')
+const cookieParser = require('cookie-parser');
+const jwt = require('jsonwebtoken');
 require('dotenv').config({ path: 'variables.env' });
 const createServer = require('./createServer');
 const db = require('./db');
 
 const server = createServer();
 server.express.use(cookieParser());
-// TODO: user express middleware to populate current user
+server.express.use((req, res, next) => {
+  const { token } = req.cookies;
+  if (token) {
+    const { userID } = jwt.verify(token, process.env.APP_SECRET);
+    req.userId = userID;
+  }
+  next();
+});
 
 server.start(
   {
